73897188405c96365d6e8bbb25c12dbf51c47781
lobos182 <lobos182@gmail.com>
2022-05-09 20:40:44 -0300

    config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Avatarjsx = (props) => {
+  const src = `https://randomuser.me/api/portraits/lego/${props.id}.jpg`
+ 
+  return (
+    <picture>
+      <img src={src} alt='lego1' />
+      {props.name}
+    </picture>
+  ) 
+  }
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Avatarjsx id={2} name={"seba"}/>
+      <Avatarjsx id={3} name={"dani"}/>
+      <Avatarjsx id={4} name={"nico"}/>
+      <Avatarjsx id={5} name={"pepe"}/>
+    </header>
+  </div>
   );
 }
 
