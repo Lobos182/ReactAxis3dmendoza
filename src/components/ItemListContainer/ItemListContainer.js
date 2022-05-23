7763fd1351a7ccb99346bf0b986cbb93e5ce3575
lobos182 <lobos182@gmail.com>
2022-05-23 20:39:28 -0300

    promesas

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,26 @@
+import './ItemListContainer.css'
+import { useState, useEffect } from 'react'
+import { getProducts } from '../../asyncmock'
+import ItemList from '../ItemList/ItemList'
+
+const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        getProducts().then(response => {
+            setProducts(response)
+        })
+    }, [])
+   
+    console.log('antes de montar')
+
+    return(
+        <div className='ItemListContainer'>
+            <h1>{ greeting }</h1>
+            {/* { products.map(product => <p key={product.id}>{product.name}</p>) } */}
+            <ItemList products={products}/>
+        </div>
+    )
+}
+
+export default ItemListContainer
